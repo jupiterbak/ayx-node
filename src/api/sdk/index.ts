4c2e39ab60163a6e96f04a1f094b8d4e8916aecd
Jupiter Bakakeu <jupiter.bakakeu@gmail.com>
2022-08-10 00:23:31 +0200

    feat: add support for different models

--- src/api/sdk/index.ts
+++ src/api/sdk/index.ts
@@ -3,6 +3,8 @@ export * from "./common/alteryx-sdk";
 export * from "./common/sdk-client";
 
 export * from "./models/index";
+export * from "./models/modelsV1";
+export * from "./models/modelsV2";
 export * from "./user/user";
 export * from "./workflow/workflow";
 export * from "./groups/group";
